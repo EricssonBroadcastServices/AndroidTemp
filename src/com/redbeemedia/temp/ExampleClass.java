48452cbf2b0a02459860bfd5d87c4654759309c7
Mattias Selin <mattias.selin@eyevinn.se>
2018-11-08 15:30:37 +0100

    Hotfix!

--- src/com/redbeemedia/temp/ExampleClass.java
+++ src/com/redbeemedia/temp/ExampleClass.java
@@ -4,10 +4,6 @@ public class ExampleClass implements IExampleClass {
 
 	@Override
 	public String getGreeting() {
-<<<<<<< HEAD
 		return "Hello!";
-=======
-		return "Hello";
->>>>>>> github_release
 	}
 }
